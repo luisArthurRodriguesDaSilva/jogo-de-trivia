c91d7f16d16cc97f9ac0f6b551a3e588d9f196dc
ConanGoodwin <conan_goodwin@hotmail.com>
2022-09-08 15:18:58 -0300

    configurado redux sem action, só store e reducer user

---
+++ src/redux/reducers/user.js
@@ -0,0 +1,13 @@
+const INITIAL_STATE = {
+  name: '',
+  email: '',
+};
+
+const user = (state = INITIAL_STATE, { type }) => {
+  switch (type) {
+  default:
+    return state;
+  }
+};
+
+export default user;
