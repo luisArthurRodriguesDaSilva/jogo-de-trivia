cc54ba9fb7256d9fe9f60d40afeb38a8a43fc48b
ConanGoodwin <conan_goodwin@hotmail.com>
2022-09-08 23:18:15 -0300

    exibe as respostas em ordem aleatoria

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -20,28 +20,79 @@ class Game extends Component {
 
   async componentDidMount() {
     const { dispatch, token } = this.props;
-    const { randomAnswer } = this.state;
+    const { indexQuestion } = this.state;
+    const RANGE = 0.5;
+    const randomIndexArray = [0, 1, 2, 3].sort(() => Math.random() - RANGE);
 
     await dispatch(fetchQuestion(token));
 
-    this.buildOrderAnswer(0);
+    const orderAnswer = this.buildOrderAnswer(indexQuestion);
+    const disorderAnswer = randomIndexArray.map((i) => orderAnswer[i]);
+    console.log(orderAnswer);
+
+    if (orderAnswer.length > 2) {
+      this.setState({ randomAnswer: disorderAnswer });
+    } else {
+      this.setState({ randomAnswer: orderAnswer });
+    }
   }
 
   buildOrderAnswer = (index) => {
     const { results } = this.props;
+    console.log(results);
     const orderAnswer = [{ answer: results[index].correct_answer, isCorrect: true }];
 
     results[index].incorrect_answers.forEach((item) => {
       orderAnswer.push({ answer: item, isCorrect: false });
     });
 
-    console.log(orderAnswer);
     return orderAnswer;
   };
 
   render() {
+    const { randomAnswer, indexQuestion } = this.state;
+    const { results } = this.props;
+    const START_INDEX = -1;
+    let indexWrongAnswer = START_INDEX;
+    console.log(randomAnswer);
+
     return (
-      <p>new game</p>
+      <main>
+        <p>new game</p>
+        <h2 data-testid="question-category">{results[indexQuestion].category}</h2>
+        <p data-testid="question-text">{results[indexQuestion].question}</p>
+        <ul data-testid="answer-options">
+          {
+            randomAnswer.map((item) => {
+              indexWrongAnswer += 1;
+
+              return (
+                <li key={ item.answer }>
+                  {
+                    (item.isCorrect)
+                      ? (
+                        <button
+                          type="button"
+                          data-testid="correct-answer"
+                        >
+                          {item.answer}
+                        </button>
+                      )
+                      : (
+                        <button
+                          type="button"
+                          data-testid={ `wrong-answer-${indexWrongAnswer}` }
+                        >
+                          {item.answer}
+                        </button>
+                      )
+                  }
+                </li>
+              );
+            })
+          }
+        </ul>
+      </main>
     );
   }
 }
@@ -49,6 +100,15 @@ class Game extends Component {
 Game.propTypes = {
   dispatch: PropTypes.func.isRequired,
   token: PropTypes.string.isRequired,
+  results: PropTypes.shape({
+    category: PropTypes.string.isRequired,
+    correct_answer: PropTypes.string.isRequired,
+    difficulty: PropTypes.string.isRequired,
+    question: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    incorrect_answers: PropTypes.arrayOf(PropTypes.string),
+
+  }).isRequired,
 };
 
 const mapStateToProps = ({ token: { tokenObj }, questions: { questionObj } }) => ({
