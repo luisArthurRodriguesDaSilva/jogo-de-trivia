d7439637ac5f5bb68ba3b9c79d03d1af54f7c348
luis arthur <luisarthurlards03@gmail.com>
2022-09-09 17:03:57 -0300

    timer

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -2,14 +2,37 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
+const ONE_SECOND = 1000;
+
 class Timer extends React.Component {
   constructor() {
     super();
+    this.state = {
+      timerCount: 30,
+    };
   }
 
+  componentDidMount() {
+    this.setTimeCount = setInterval(() => {
+      this.setState((previewState) => ({ timerCount: previewState.timerCount - 1 }));
+    }, ONE_SECOND);
+  }
+
+  timeIsEnd = () => {
+    const { handleClickAnswer } = this.props;
+    clearInterval(this.setTimeCount);
+    handleClickAnswer();
+  };
+
   render() {
-    return (<h1>Timer</h1>);
+    const { timerCount } = this.state;
+    if (timerCount === 0) { this.timeIsEnd(); }
+    return (<h1>{timerCount}</h1>);
   }
 }
 
-export default connect(mapStateToProps)(Timer);
+Timer.propTypes = {
+  handleClickAnswer: PropTypes.func.isRequired,
+};
+
+export default connect()(Timer);
