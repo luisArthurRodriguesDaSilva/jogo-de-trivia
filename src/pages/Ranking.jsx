96bd0f31ec6c37f10ee7edda26244c90add08e6d
ConanGoodwin <conan_goodwin@hotmail.com>
2022-09-10 13:02:30 -0300

    add dispatch zerando o placar

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { userScore } from '../redux/actions';
 
 class Ranking extends Component {
-  render() {
-    const { history } = this.props;
+  handleClickGoHome = () => {
+    const { history, dispatch } = this.props;
 
+    dispatch(userScore(0));
+    history.push('/');
+  };
+
+  render() {
     return (
       <section>
         <h1 data-testid="ranking-title">Ranking</h1>
         <button
           type="button"
           data-testid="btn-go-home"
-          onClick={ () => history.push('/') }
+          onClick={ this.handleClickGoHome }
         >
           Tela Inicial
         </button>
@@ -21,9 +28,10 @@ class Ranking extends Component {
 }
 
 Ranking.propTypes = {
+  dispatch: PropTypes.func.isRequired,
   history: PropTypes.shape({
     push: PropTypes.func.isRequired,
   }).isRequired,
 };
 
-export default Ranking;
+export default connect()(Ranking);
