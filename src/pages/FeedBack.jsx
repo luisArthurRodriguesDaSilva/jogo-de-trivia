66c406899063689b4fa448eeddfb6be4019d6bf3
Marcos Paulo <dev.marcospereira@gmail.com>
2022-09-12 13:45:55 -0300

    Requisito 12,15 - 14 funciona mais não passa no avaliador

--- src/pages/FeedBack.jsx
+++ src/pages/FeedBack.jsx
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import Header from '../components/Header';
 
 const VALUE_FEEDBACK = 3;
 
 class FeedBack extends Component {
   render() {
-    const { totalHits, history } = this.props;
+    const { totalHits, history, totalScore } = this.props;
 
     return (
       <div>
+        <Header />
         {totalHits >= VALUE_FEEDBACK
           ? <p data-testid="feedback-text">Well Done!</p>
           : <p data-testid="feedback-text">Could be better...</p>}
+        {/* <h4>Total de perguntas certas:</h4> */}
+        <p data-testid="feedback-total-score">{totalScore}</p>
+        {/* <h4>Total de pontos:</h4> */}
+        <p data-testid="feedback-total-question">{totalHits}</p>
         <button
           type="button"
           data-testid="btn-ranking"
@@ -20,13 +26,21 @@ class FeedBack extends Component {
         >
           Ranking
         </button>
+        <button
+          type="button"
+          data-testid="btn-play-again"
+          onClick={ () => history.push('/') }
+        >
+          Play Again
+        </button>
       </div>
     );
   }
 }
 
-const mapStateToProps = ({ score }) => ({
+const mapStateToProps = ({ score, player }) => ({
   ...score,
+  totalScore: player.score,
 });
 
 FeedBack.propTypes = {
@@ -34,6 +48,7 @@ FeedBack.propTypes = {
   history: PropTypes.shape({
     push: PropTypes.func.isRequired,
   }).isRequired,
+  totalScore: PropTypes.number.isRequired,
 };
 
 export default connect(mapStateToProps)(FeedBack);
