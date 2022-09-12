da32899f9f5633f7bf86caf5fe31c5b8b7750d23
luis arthur <luisarthurlards03@gmail.com>
2022-09-12 17:49:39 -0300

    mocks + render with router velho

--- src/tests/helpers/renderWithRouterAndRedux.js
+++ src/tests/helpers/renderWithRouterAndRedux.js
@@ -1,27 +1,31 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
+import { render } from '@testing-library/react';
+
 import { Provider } from 'react-redux';
-import {  legacy_createStore as createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import reducer from '../../redux/reducers';
+import rootReducer from '../../redux/reducers';
 
-export const renderWithRouterAndRedux = (component, initialState, route = '/') => {
-  const store = createStore(reducer, initialState, applyMiddleware(thunk));
-  const history = createMemoryHistory({ initialEntries: [route] });
-
-  return {
-    ...render(
+const renderWithRouterAndRedux = (
+  component,
+  {
+    initialState = {},
+    store = createStore(rootReducer, initialState, applyMiddleware(thunk)),
+    initialEntries = ['/'],
+    history = createMemoryHistory({ initialEntries }),
+  } = {},
+) => ({
+  ...render(
+    <Router history={ history }>
       <Provider store={ store }>
-        <Router history={ history }>
-          {component}
-        </Router>
-      </Provider>,
-    ),
-    history,
-    store,
-  };
-};
+        {component}
+      </Provider>
+    </Router>,
+  ),
+  history,
+  store,
+});
 
 export default renderWithRouterAndRedux;
