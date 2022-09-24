d9e198dc16812ecf69ed9cd6d7c5dd46c467c482
ConanGoodwin <conan_goodwin@hotmail.com>
2022-09-24 15:30:52 -0300

    add estilo a tela de configurações

--- src/pages/settings.jsx
+++ src/pages/settings.jsx
@@ -1,11 +1,58 @@
 import React, { Component } from 'react';
+import './style/settings.css';
 
 class Settings extends Component {
   render() {
     return (
-      <div>
-        <h1 data-testid="settings-title">Configuração</h1>
-      </div>
+      <form className="sett_ings">
+        <h1 data-testid="settings-title">Configurações</h1>
+
+        <div className="control has-icons-left marg">
+          <div className="select is-link ">
+            <select className="mor">
+              <option value="a">Categoria</option>
+              <option value="a">A</option>
+            </select>
+          </div>
+          <span className="icon is-left">
+            <i className="fa-solid fa-clipboard" />
+          </span>
+        </div>
+
+        <div className="control has-icons-left marg">
+          <div className="select is-link">
+            <select className="mor">
+              <option value="1">Dificuldade</option>
+              <option value="2">2</option>
+            </select>
+          </div>
+          <span className="icon is-left">
+            <i className="fa-solid fa-bomb" />
+          </span>
+        </div>
+
+        <div className="control has-icons-left marg">
+          <div className="select is-link">
+            <select className="mor">
+              <option value="a">Tipo</option>
+              <option value="a">A</option>
+            </select>
+          </div>
+          <span className="icon is-left">
+            <i className="fas fa-globe" />
+          </span>
+        </div>
+
+        <button
+          type="submit"
+          className="button is-link
+             is-responsive is-outlined has-tooltip-arrow has-tooltip-bottom"
+          data-tooltip="Incia o jogo"
+          data-testid="btn-play"
+        >
+          Play
+        </button>
+      </form>
     );
   }
 }
